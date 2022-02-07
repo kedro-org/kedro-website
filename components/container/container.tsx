4b2c8c0ebab033cdf1d6adef00bd8f1ac6d418aa
rashidakanchwala <37628668+rashidakanchwala@users.noreply.github.com>
2022-02-07 18:54:36 +0000

    Scaffolding Kedro Website (#1)

---
+++ components/container/container.tsx
@@ -0,0 +1,5 @@
+import containerStyle from './container.module.scss';
+
+export default function Container() {
+  return <h1 className={containerStyle.title}>Hello World</h1>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Container from '../components/container';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Kedro</title>
+      </Head>
+      <Container />
+    </div>
+  );
+};
+
+export default Home;
