83c42a33739561a405445e018ca27db50125e2d5
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-02-09 09:35:36 +0000

    Hero module

--- components/media/media.tsx
+++ components/media/media.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Media(media: any) {
   if (media.source.src.includes('webp')) {
     return <Image src={media.source} layout="intrinsic"></Image>;
-  } else if (media.source.src.includes('mp4')) {
-    return <video src={media.source}></video>;
+  } else {media.source.src.includes('mp4');}
+  {
+    return null;
   }
-  return <></>;
 }
