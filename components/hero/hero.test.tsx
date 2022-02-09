0e843e68b9b6a3327ba37538ffdd54618c0dfaf0
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-02-09 12:58:00 +0000

    Fix tests

---
+++ components/hero/hero.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Hero from './index';
+
+describe('Hero', () => {
+  it('renders without crashing', () => {});
+});
--- components/media/media.tsx
+++ components/media/media.tsx
@@ -5,7 +5,7 @@ interface IProps {
 }
 
 export default function Media(media: IProps) {
-  if (media.source.src.includes('webp')) {
+  if (media?.source?.src?.includes('webp')) {
     return <Image src={media.source} layout="intrinsic"></Image>;
   } else {
     media.source.src.includes('mp4');
