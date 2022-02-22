306400b4c4938bf24c24139b3afa8425387edc83
Tynan DeBold <thdebold@gmail.com>
2022-02-22 18:01:54 +0000

    mobile styling

--- components/why-kedro/why-kedro.tsx
+++ components/why-kedro/why-kedro.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { tabData } from './why-kedro-data';
 
-import image from '../../public/images/whykedro.png';
+import image from '../../public/images/why-kedro.png';
 import Media from '../media';
 
 import style from './why-kedro.module.scss';
@@ -11,17 +11,17 @@ export default function WhyKedro() {
   const [selectedTab, setSelectedTab] = useState(tabData[0]);
 
   return (
-    <div className={style.window}>
+    <div className={style.container}>
       <h3 className={style.title}>Why Kedro?</h3>
-      <div className={style.nav}>
-        <ul className={style.ul}>
+      <div className={style.tabsWrapper}>
+        <ul className={style.list}>
           {tabData.map((item) => (
             <li
               key={item.label}
               className={
                 item === selectedTab
-                  ? `${style.selected} ${style.li}`
-                  : style.li
+                  ? `${style.selected} ${style.listItem}`
+                  : style.listItem
               }
               onClick={() => setSelectedTab(item)}
             >
@@ -33,20 +33,18 @@ export default function WhyKedro() {
           ))}
         </ul>
       </div>
-      <div className={style.main}>
-        <AnimatePresence exitBeforeEnter>
-          <motion.p
-            animate={{ opacity: 1, y: 0 }}
-            className={style.conceptContent}
-            exit={{ opacity: 0, y: -20 }}
-            initial={{ opacity: 0, y: 20 }}
-            key={selectedTab ? selectedTab.label : null}
-            transition={{ duration: 0.1 }}
-          >
-            {selectedTab.description}
-          </motion.p>
-        </AnimatePresence>
-      </div>
+      <AnimatePresence exitBeforeEnter>
+        <motion.p
+          animate={{ opacity: 1, y: 0 }}
+          className={style.tabContent}
+          exit={{ opacity: 0, y: -20 }}
+          initial={{ opacity: 0, y: 20 }}
+          key={selectedTab ? selectedTab.label : null}
+          transition={{ duration: 0.1 }}
+        >
+          {selectedTab.description}
+        </motion.p>
+      </AnimatePresence>
       <Media source={image} />
     </div>
   );
