f01f24b4d0a2c1f9c21628948cd2f1edda304852
Tynan DeBold <thdebold@gmail.com>
2022-02-22 16:54:34 +0000

    tab working on desktop

---
+++ components/why-kedro/why-kedro-data.ts
@@ -0,0 +1,26 @@
+export const tabData = [
+  {
+    description:
+      'Puts the "engineering" back into data science because it borrows concepts from software engineering and applies them to machine-learning code. It is the foundation for clean, data science code.',
+    icon: '🍅',
+    label: 'Machine Learning Engineering',
+  },
+  {
+    description:
+      'Provides the scaffolding to build more complex data and machine-learning pipelines. In addition, there\'s a focus on spending less time on the tedious "plumbing" required to maintain data science code; this means that you have more time to solve new problems.',
+    icon: '🥬',
+    label: 'Handles Complexity',
+  },
+  {
+    description:
+      'Standardises team workflows; the modular structure of Kedro facilitates a higher level of collaboration when teams solve problems together.',
+    icon: '🧀',
+    label: 'Standardisation',
+  },
+  {
+    description:
+      'Makes a seamless transition from development to production, as you can write quick, throw-away exploratory code and transition to maintainable, easy-to-share, code experiments quickly.',
+    icon: '🥕',
+    label: 'Production-Ready',
+  },
+];
--- components/why-kedro/why-kedro.tsx
+++ components/why-kedro/why-kedro.tsx
@@ -1,31 +1,60 @@
-import style from './why-kedro.module.scss';
-import Tabs from '../ui/tabs';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { tabData } from './why-kedro-data';
+
 import image from '../../public/images/whykedro.png';
 import Media from '../media';
 
+import style from './why-kedro.module.scss';
+
 export default function WhyKedro() {
+  const [selectedTab, setSelectedTab] = useState(tabData[0]);
+
   return (
-    <div className={style['why']}>
-      <div className={style['why-container']}>
-        <h1 className={style['why-title']}>Why Kedro?</h1>
-        <div className={style['why-tab']}>
-          <Tabs>
-            <div data-label="Standardisation" className={style['why-tab-info']}>
-              <p className={style['why-tab-summary']}>
-                Kedro Standardises team workflows; the modular structure of
-                Kedro facilitates a higher level of collaboration when teams
-                solve problems together.
-              </p>
-              <Media source={image} className={style['why-tab-media']}></Media>
-            </div>
-            <div data-label="Croc">
-              After while, <em>Crocodile</em>!
-            </div>
-            <div data-label="Sarcosuchus">
-              Nothing to see here, this tab is <em>extinct</em>!
-            </div>
-          </Tabs>
-        </div>
+    <div className={style.window}>
+      <h3 className={style.title}>Why Kedro?</h3>
+      <div className={style.nav}>
+        <ul className={style.ul}>
+          {tabData.map((item) => (
+            <li
+              key={item.label}
+              className={
+                item === selectedTab
+                  ? `${style.selected} ${style.li}`
+                  : style.li
+              }
+              onClick={() => setSelectedTab(item)}
+            >
+              {item.label}
+              {item === selectedTab ? (
+                <motion.div className={style.underline} layoutId="underline" />
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={style.main}>
+        <AnimatePresence exitBeforeEnter>
+          <React.Fragment key={selectedTab ? selectedTab.label : null}>
+            <motion.p
+              animate={{ opacity: 1, y: 0 }}
+              className={style.conceptContent}
+              exit={{ opacity: 0, y: -20 }}
+              initial={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.1 }}
+            >
+              {selectedTab.description}
+            </motion.p>
+            <motion.div
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              initial={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.15 }}
+            >
+              {selectedTab ? selectedTab.icon : null}
+            </motion.div>
+          </React.Fragment>
+        </AnimatePresence>
       </div>
     </div>
   );
