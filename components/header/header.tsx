2b2d55c75d0567ff022ca56ebe38de249a2d13c8
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-03-04 13:32:06 +0000

    Ready to review

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
 import style from './header.module.scss';
 import KedroLogo from '../../public/assets/logos/kedro';
 import DiscordLogo from '../../public/assets/logos/discord';
@@ -11,44 +10,50 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className={style.container}>
-      <div className={style.logo}>
-        <KedroLogo />
-        <h4 className={style.logoText}>Kedro</h4>
-      </div>
-      <nav
-        className={menuOpen ? `${style.menu} ${style.active}` : style.menu}
-        onClick={() => setMenuOpen(false)}
-      >
-        <a href="#why-kedro" className={style.link}>
-          Why Kedro
-        </a>
-        <a href="#features" className={style.link}>
-          Features
-        </a>
-        <a href="#" className={style.link}>
-          Get Started
-        </a>
-        <a href="#" className={style.link}>
-          Documentation
-        </a>
-        <div className={style.iconLinks}>
-          <a className={style.link} href="https://github.com/kedro-org">
-            <DiscordLogo />
-            <span className={style.iconText}>Discord</span>
-          </a>
-          <a className={style.githubLink} href="https://github.com/kedro-org">
-            <GithubLogo />
-            <span className={style.iconText}> Github</span>
-          </a>
+      <div className={style.header}>
+        <div className={style.logo}>
+          <KedroLogo />
+          <h4 className={style.logoText}>Kedro</h4>
         </div>
-      </nav>
-      <button
-        aria-label={menuOpen ? 'open' : 'close'}
-        className={style.burger}
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        {menuOpen ? <CloseIcon /> : <BurgerIcon />}
-      </button>
+        {/*TO DO - Update All Links*/}
+        <nav
+          className={menuOpen ? `${style.menu} ${style.active}` : style.menu}
+          onClick={() => setMenuOpen(false)}
+        >
+          <a href="#why-kedro" className={style.link}>
+            Why Kedro
+          </a>
+          <a href="#features" className={style.link}>
+            Features
+          </a>
+          <a href="#" className={style.link}>
+            Get Started
+          </a>
+          <a href="#" className={style.link}>
+            Documentation
+          </a>
+          <div className={style.iconLinks}>
+            <a className={style.link} href="https://github.com/kedro-org">
+              <DiscordLogo />
+              <span className={style.iconText}>Discord</span>
+            </a>
+            <a
+              className={style.githubLink}
+              href="https://discord.com/kedro-org"
+            >
+              <GithubLogo />
+              <span className={style.iconText}> Github</span>
+            </a>
+          </div>
+        </nav>
+        <button
+          aria-label={menuOpen ? 'open' : 'close'}
+          className={style.burger}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <CloseIcon /> : <BurgerIcon />}
+        </button>
+      </div>
     </div>
   );
 }
