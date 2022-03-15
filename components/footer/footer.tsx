50fd4335438aab22c00b07443b66bfcbdb0e0483
Tynan DeBold <thdebold@gmail.com>
2022-03-15 16:46:39 +0000

    spacing fixes
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,69 +4,66 @@ import style from './footer.module.scss';
 
 export default function Footer() {
   return (
-    <section id="footer">
-      <div className={style.container}>
-        <div className={style.footer}>
-          <a href="#" className={style.logo}>
-            <Image
-              alt="KedroLogo"
-              src="/images/kedro-logo.svg"
-              width={30}
-              height={30}
-            />
-            <h4 className={style.logoText}>Kedro</h4>
+    <section id="footer" className={style.outer}>
+      <div className={style.inner}>
+        <a href="#" className={style.logo}>
+          <Image
+            alt="KedroLogo"
+            src="/images/kedro-logo.svg"
+            width={30}
+            height={30}
+          />
+          <h4 className={style.logoText}>Kedro</h4>
+        </a>
+        <nav className={style.menu}>
+          <a href="#why-kedro" className={style.link}>
+            Why Kedro?
           </a>
-          {/*TO DO - Update All Links*/}
-          <nav className={style.menu}>
-            <a href="#why-kedro" className={style.link}>
-              Why Kedro?
-            </a>
-            <a href="#features" className={style.link}>
-              Features
-            </a>
-            <a href="#" className={style.link}>
-              Get Started
-            </a>
+          <a href="#features" className={style.link}>
+            Features
+          </a>
+          <a href="#" className={style.link}>
+            Get Started
+          </a>
+          <a
+            href="https://kedro.readthedocs.io/en/stable/"
+            className={style.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+          <div className={style.iconLinks}>
             <a
-              href="https://kedro.readthedocs.io/en/stable/"
               className={style.link}
+              href="https://discord.gg/4qeKKspFf8"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Documentation
+              <Image
+                alt="DiscordLogo"
+                src="/images/discord.svg"
+                width={30}
+                height={30}
+              />
+              <span className={style.iconText}>Discord</span>
             </a>
-            <div className={style.iconLinks}>
-              <a
-                className={style.link}
-                href="https://discord.gg/4qeKKspFf8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  alt="DiscordLogo"
-                  src="/images/discord.svg"
-                  width={30}
-                  height={30}
-                />
-                <span className={style.iconText}>Discord</span>
-              </a>
-              <a
-                className={style.link}
-                href="https://github.com/kedro-org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  alt="GithubLogo"
-                  src="/images/github.svg"
-                  width={30}
-                  height={30}
-                />
-                <span className={style.iconText}> Github</span>
-              </a>
-            </div>
-          </nav>
-        </div>
+            <a
+              className={style.link}
+              href="https://github.com/kedro-org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                alt="GithubLogo"
+                src="/images/github.svg"
+                width={30}
+                height={30}
+              />
+              <span className={style.iconText}> Github</span>
+            </a>
+          </div>
+        </nav>
       </div>
     </section>
   );
