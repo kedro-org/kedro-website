f8f6e05bfbcfcc9621c35c5ac51aef9e12433457
Tynan DeBold <thdebold@gmail.com>
2022-03-17 16:06:46 +0000

    animation nearly there
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- components/testimonials/testimonials.tsx
+++ components/testimonials/testimonials.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { testimonials } from './testimonial-content';
 
 import Image from 'next/image';
@@ -36,15 +36,25 @@ const UserDescription = ({ testimonial, view }: Props) => {
 };
 
 const variants = {
-  enter: (direction: number) => {
-    return {
-      opacity: 0,
-      x: direction > 0 ? 1000 : -1000,
-    };
+  enter: {
+    opacity: 0,
+    x: 0,
   },
   center: {
     opacity: 1,
+    transition: {
+      delay: 0.4,
+      duration: 0.25,
+    },
     x: 0,
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      delay: 0,
+      duration: 0.4,
+    },
+    x: -100,
   },
 };
 
@@ -61,63 +71,62 @@ export default function Testimonials() {
       <h3 className={style.sectionTitle}>Testimonials</h3>
       <div className={style.inner}>
         <div className={style.carousel}>
-          <motion.div
-            animate="center"
-            className={style.slide}
-            custom={index}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragElastic={1}
-            exit="exit"
-            initial="enter"
-            key={index}
-            onDragEnd={(e, { offset, velocity }) => {
-              const swipe = swipePower(offset.x, velocity.x);
+          <AnimatePresence initial={false}>
+            <motion.div
+              animate="center"
+              className={style.slide}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              exit="exit"
+              initial="enter"
+              key={testimonials[index].user}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
 
-              if (swipe < -swipeConfidenceThreshold) {
-                setIndex(index + 1 === testimonials.length ? index : index + 1);
-              } else if (swipe > swipeConfidenceThreshold) {
-                setIndex(index - 1 === -1 ? index : index - 1);
-              }
-            }}
-            transition={{
-              x: {
-                damping: 50,
-                duration: 0.75,
-                stiffness: 500,
-                type: 'spring',
-              },
-              opacity: { duration: 0.75 },
-            }}
-            variants={variants}
-          >
-            <UserDescription testimonial={testimonials[index]} view="mobile" />
-            <div className={style.userImage}>
-              <Media
-                alt={testimonials[index].user}
-                image={testimonials[index].userImage}
-                layout="fill"
-              />
-            </div>
-            <div>
+                if (swipe < -swipeConfidenceThreshold) {
+                  setIndex(
+                    index + 1 === testimonials.length ? index : index + 1
+                  );
+                } else if (swipe > swipeConfidenceThreshold) {
+                  setIndex(index - 1 === -1 ? index : index - 1);
+                }
+              }}
+              variants={variants}
+            >
               <UserDescription
                 testimonial={testimonials[index]}
-                view="desktop"
+                view="mobile"
               />
-              <h3 className={style.headline}>{testimonials[index].headline}</h3>
-              <p className={style.quote}>
-                &#34;{testimonials[index].text}&#34;
-              </p>
-              <a
-                className={style.link}
-                href={testimonials[index].linkUrl}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                {testimonials[index].linkText}
-              </a>
-            </div>
-          </motion.div>
+              <div className={style.userImage}>
+                <Media
+                  alt={testimonials[index].user}
+                  image={testimonials[index].userImage}
+                  layout="fill"
+                />
+              </div>
+              <div>
+                <UserDescription
+                  testimonial={testimonials[index]}
+                  view="desktop"
+                />
+                <h3 className={style.headline}>
+                  {testimonials[index].headline}
+                </h3>
+                <p className={style.quote}>
+                  &#34;{testimonials[index].text}&#34;
+                </p>
+                <a
+                  className={style.link}
+                  href={testimonials[index].linkUrl}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {testimonials[index].linkText}
+                </a>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
       <div className={style.dots}>
