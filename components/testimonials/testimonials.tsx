5958b5d1613900275868df6fb91c077e374a6934
Tynan DeBold <thdebold@gmail.com>
2022-03-17 14:07:32 +0000

    closer with the animation
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- components/testimonials/testimonials.tsx
+++ components/testimonials/testimonials.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import { animate, motion, useMotionValue } from 'framer-motion';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { testimonials } from './testimonial-content';
 
 import Image from 'next/image';
@@ -35,6 +35,19 @@ const UserDescription = ({ testimonial, view }: Props) => {
   );
 };
 
+const variants = {
+  enter: (direction: number) => {
+    return {
+      opacity: 0,
+      x: direction > 0 ? 1000 : -1000,
+    };
+  },
+  center: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
@@ -42,68 +55,69 @@ const swipePower = (offset: number, velocity: number) => {
 
 export default function Testimonials() {
   const [index, setIndex] = useState(0);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const x = useMotionValue(0);
-
-  useEffect(() => {
-    const newToValue = () => {
-      return index * containerRef.current?.clientWidth * -1;
-    };
-
-    animate(x, newToValue(), {
-      bounce: 0.05,
-      damping: 30,
-      stiffness: 500,
-      type: 'spring',
-    });
-  }, [index, x]);
 
   return (
     <section className={style.outer}>
       <h3 className={style.sectionTitle}>Testimonials</h3>
       <div className={style.inner}>
-        <div className={style.carousel} ref={containerRef}>
-          {testimonials.map((testimonial: Testimonial, i: number) => (
-            <motion.div
-              className={style.slide}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              dragElastic={1}
-              onDragEnd={(e, { offset, velocity }) => {
-                const swipe = swipePower(offset.x, velocity.x);
+        <div className={style.carousel}>
+          <motion.div
+            animate="center"
+            className={style.slide}
+            custom={index}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            exit="exit"
+            initial="enter"
+            key={index}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
 
-                if (swipe < -swipeConfidenceThreshold) {
-                  setIndex(index + 1);
-                } else if (swipe > swipeConfidenceThreshold) {
-                  setIndex(index - 1);
-                }
-              }}
-              key={i}
-              style={{ x }}
-            >
-              <UserDescription testimonial={testimonial} view="mobile" />
-              <div className={style.userImage}>
-                <Media
-                  alt={testimonial.user}
-                  image={testimonial.userImage}
-                  layout="fill"
-                />
-              </div>
-              <div>
-                <UserDescription testimonial={testimonial} view="desktop" />
-                <h3 className={style.headline}>{testimonial.headline}</h3>
-                <p className={style.quote}>&#34;{testimonial.text}&#34;</p>
-                <a
-                  className={style.link}
-                  href={testimonial.linkUrl}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {testimonial.linkText}
-                </a>
-              </div>
-            </motion.div>
-          ))}
+              if (swipe < -swipeConfidenceThreshold) {
+                setIndex(index + 1 === testimonials.length ? index : index + 1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                setIndex(index - 1 === -1 ? index : index - 1);
+              }
+            }}
+            transition={{
+              x: {
+                damping: 50,
+                duration: 0.75,
+                stiffness: 500,
+                type: 'spring',
+              },
+              opacity: { duration: 0.75 },
+            }}
+            variants={variants}
+          >
+            <UserDescription testimonial={testimonials[index]} view="mobile" />
+            <div className={style.userImage}>
+              <Media
+                alt={testimonials[index].user}
+                image={testimonials[index].userImage}
+                layout="fill"
+              />
+            </div>
+            <div>
+              <UserDescription
+                testimonial={testimonials[index]}
+                view="desktop"
+              />
+              <h3 className={style.headline}>{testimonials[index].headline}</h3>
+              <p className={style.quote}>
+                &#34;{testimonials[index].text}&#34;
+              </p>
+              <a
+                className={style.link}
+                href={testimonials[index].linkUrl}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {testimonials[index].linkText}
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
       <div className={style.dots}>
