82d63851834101b95e83adceb16692fd16e93d10
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-03-25 13:56:25 +0000

    Added tests

--- components/companies-using-kedro/companies-using-kedro.test.tsx
+++ components/companies-using-kedro/companies-using-kedro.test.tsx
@@ -4,8 +4,22 @@ import '@testing-library/jest-dom';
 
 import CompaniesUsingKedro from './index';
 
+jest.useFakeTimers();
+
 describe('Company using Kedro', () => {
   it('renders without crashing', () => {
     render(<CompaniesUsingKedro />);
+  });
+
+  it('works with list item animation', () => {
+    render(<CompaniesUsingKedro />);
+    const listItems = screen.getAllByRole('listitem');
+
+    expect(listItems[2]).toHaveAttribute('class', 'active');
+
+    jest.advanceTimersByTime(3000);
+
+    expect(listItems[2]).toHaveAttribute('class', '');
+    expect(listItems[3]).toHaveAttribute('class', 'active');
   });
 });
