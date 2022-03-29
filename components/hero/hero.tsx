cc797e0e9a090a6a1167510dcb783e3c8feedc7d
Tynan DeBold <thdebold@gmail.com>
2022-03-29 19:00:46 +0100

    getting there
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,14 +1,70 @@
+import { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import Media from '../media';
 import hero from '../../public/images/hero.webp';
 
 import style from './hero.module.scss';
 
+const variants = {
+  enter: (wordIndex: number) => {
+    return {
+      opacity: 0,
+      y: wordIndex === 0 ? 100 : -100,
+    };
+  },
+  center: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+    y: 0,
+  },
+  exit: (wordIndex: number) => {
+    return {
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+      y: wordIndex === 0 ? -100 : 100,
+    };
+  },
+};
+
+const headerText = ['Maintainable', 'Modular'];
+
 export default function Hero() {
+  const [wordIndex, setWordIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setWordIndex(wordIndex === 0 ? 1 : 0);
+    }, 5500);
+
+    return () => clearInterval(interval);
+  }, [wordIndex]);
+
   return (
     <section className={style.container}>
       <div className={style.row}>
         <div className={style.info}>
-          <h1 className={style.title}>Maintainable data science solved</h1>
+          <h1 className={style.title}>
+            <div style={{ height: 90, overflow: 'hidden' }}>
+              <AnimatePresence custom={wordIndex} initial={false}>
+                <motion.div
+                  animate="center"
+                  custom={wordIndex}
+                  exit="exit"
+                  initial="enter"
+                  key={wordIndex}
+                  variants={variants}
+                >
+                  {headerText[wordIndex]}
+                </motion.div>
+              </AnimatePresence>{' '}
+            </div>
+            data science solved
+          </h1>
           <h2 className={style.subtitle}>
             Kedro is an open-sourced Python framework for creating maintainable
             and modular data science code.
