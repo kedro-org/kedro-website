22e1acb6c1a963d76875140c18a4ba59c9e509ec
Tynan DeBold <thdebold@gmail.com>
2022-04-06 18:07:40 +0100

    test hooks

--- components/testimonials/testimonials.test.tsx
+++ components/testimonials/testimonials.test.tsx
@@ -15,7 +15,7 @@ describe('Testimonials', () => {
 
     const carouselNavigation = screen.getByLabelText('carousel-nav');
 
-    expect(carouselNavigation.children).toHaveLength(5);
+    expect(carouselNavigation.children).toHaveLength(50);
     expect(
       carouselNavigation.firstElementChild.classList.contains('active')
     ).toBe(true);
