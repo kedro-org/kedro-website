61f072671fbb151a1074bcc27c355b84ec632681
Tynan DeBold <thdebold@gmail.com>
2022-04-28 16:07:15 +0100

    add social image and meta tags
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,21 @@ export default function Document() {
           name="description"
           content="Kedro is an open-source Python framework for creating reproducible, maintainable and modular data-science code. It borrows concepts from software engineering and applies them to machine-learning code."
         />
+
+        <meta property="og:title" content="Kedro" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/kedro-social-image.png" />
+        <meta property="og:url" content="https://kedro.org/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="og:description"
+          content="A Python framework for creating reproducible, maintainable and modular data science code."
+        />
+        <meta property="og:site_name" content="Kedro" />
+        <meta
+          name="twitter:image:alt"
+          content="A Python framework for creating reproducible, maintainable and modular data science code."
+        />
       </Head>
       <body>
         <Main />
