cd8bbb6eb6e1483d517e976078bdeec095533d89
Tynan DeBold <thdebold@gmail.com>
2022-06-28 16:26:23 +0100

    add links to the company names and articles to their use of kedro
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- components/companies-using-kedro/companies-using-kedro.tsx
+++ components/companies-using-kedro/companies-using-kedro.tsx
@@ -1,24 +1,7 @@
 import React, { useEffect } from 'react';
-import style from './companies-using-kedro.module.scss';
+import { companies } from './companies-using-kedro-content';
 
-export const companies = [
-  'Telkomsel',
-  'NASA',
-  'Belfius',
-  'Sber',
-  'Augment Partners',
-  'McKinsey & Company',
-  'GetInData',
-  'Indicium',
-  'NHS',
-  'GMO',
-  'QuantumBlack',
-  'XP',
-  'AI Singapore',
-  'Helvetas',
-  'Leapfrog',
-  'Naranja',
-];
+import style from './companies-using-kedro.module.scss';
 
 export default function CompaniesUsingKedro() {
   useEffect(() => {
@@ -69,15 +52,53 @@ export default function CompaniesUsingKedro() {
         <div className={style.right}>
           <div className={style.animation}>
             <ul>
-              <li>Leapfrog</li>
-              <li>Naranja</li>
+              <li>
+                <a
+                  href="https://www.lftechnology.com/blog/ai-pipeline-kedro/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Leapfrog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://medium.com/quantumblack/odelsa-uses-kedro-for-social-good-3c7e7fc20306"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Naranja
+                </a>
+              </li>
               {companies.map((company, i) => (
                 <li key={i} className={i == 0 ? style.active : ''}>
-                  {company}
+                  <a
+                    href={company.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {company.name}{' '}
+                  </a>
                 </li>
               ))}
-              <li>Beamery</li>
-              <li>Telkomsel</li>
+              <li>
+                <a
+                  href="https://medium.com/hacking-talent/production-code-for-data-science-and-our-experience-with-kedro-60bb69934d1f"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Beamery
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://medium.com/life-at-telkomsel/how-we-build-a-production-grade-data-pipeline-7004e56c8c98"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Telkomsel
+                </a>
+              </li>
             </ul>
           </div>
           <div className={style.animationMask}></div>
