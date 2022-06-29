5e1bdb9fc18420c1760affc1e7b4e0740ffcd0e1
Tynan DeBold <thdebold@gmail.com>
2022-06-29 11:34:44 +0100

    Add the NHS to the used and loved by section (#53)
    * add the nhs to the used and loved by section
    
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>
    
    * add links to the company names and articles to their use of kedro
    
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>
    
    * Update NHS naming
    
    Co-authored-by: Yetunde Dada <43755008+yetudada@users.noreply.github.com>
    
    Co-authored-by: Yetunde Dada <43755008+yetudada@users.noreply.github.com>

---
+++ components/companies-using-kedro/companies-using-kedro-content.tsx
@@ -0,0 +1,64 @@
+export const companies = [
+  {
+    name: 'Telkomsel',
+    link: 'https://medium.com/life-at-telkomsel/how-we-build-a-production-grade-data-pipeline-7004e56c8c98',
+  },
+  {
+    name: 'Beamery',
+    link: 'https://medium.com/hacking-talent/production-code-for-data-science-and-our-experience-with-kedro-60bb69934d1f',
+  },
+  { name: 'NASA', link: 'https://github.com/nasa/ML-airport-configuration' },
+  {
+    name: 'Belfius',
+    link: 'https://www.linkedin.com/posts/vangansen_mlops-machinelearning-kedro-activity-6772379995953238016-JUmo/',
+  },
+  {
+    name: 'Sber',
+    link: 'https://www.linkedin.com/posts/seleznev-artem_welcome-to-kedros-documentation-kedro-activity-6767523561109385216-woTt/?ref=morioh.com&utm_source=morioh.com',
+  },
+  {
+    name: 'Augment Partners',
+    link: 'https://www.linkedin.com/posts/augment-partners_kedro-cheat-sheet-by-augment-activity-6858927624631283712-Ivqk/?ref=morioh.com&utm_source=morioh.com',
+  },
+  {
+    name: 'McKinsey & Company',
+    link: 'https://www.mckinsey.com/alumni/news-and-insights/global-news/firm-news/kedro-from-proprietary-to-open-source',
+  },
+  {
+    name: 'GetInData',
+    link: 'https://getindata.com/blog/running-machine-learning-pipelines-kedro-kubeflow-airflow',
+  },
+  {
+    name: 'Indicium',
+    link: 'https://medium.com/indiciumtech/how-to-build-models-as-products-using-mlops-part-2-machine-learning-pipelines-with-kedro-10337c48de92',
+  },
+  {
+    name: 'NHS AI Lab',
+    link: 'https://nhsx.github.io/skunkworks/synthetic-data-pipeline',
+  },
+  {
+    name: 'GMO',
+    link: 'https://recruit.gmo.jp/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/blog/kedro_and_mlflow_tracking/',
+  },
+  {
+    name: 'QuantumBlack',
+    link: 'https://medium.com/quantumblack/introducing-kedro-the-open-source-library-for-production-ready-machine-learning-code-d1c6d26ce2cf',
+  },
+  { name: 'XP', link: 'https://youtu.be/wgnGOVNkXqU?t=2210' },
+  {
+    name: 'AI Singapore',
+    link: 'https://makerspace.aisingapore.org/2020/08/leveraging-kedro-in-100e/',
+  },
+  {
+    name: 'Helvetas',
+    link: 'https://www.linkedin.com/posts/lionel-trebuchon_mlflow-kedro-ml-ugcPost-6747074322164154368-umKw',
+  },
+  {
+    name: 'Leapfrog',
+    link: 'https://www.lftechnology.com/blog/ai-pipeline-kedro/',
+  },
+  {
+    name: 'Naranja',
+    link: 'https://medium.com/quantumblack/odelsa-uses-kedro-for-social-good-3c7e7fc20306',
+  },
+];
