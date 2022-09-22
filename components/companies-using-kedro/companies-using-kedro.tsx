303922bb7d4d7de3f6f5008d953ef4aae296d64d
huongg <huongg1409@gmail.com>
2022-09-22 16:36:56 +0100

    remove left and right
    Signed-off-by: huongg <huongg1409@gmail.com>

--- components/companies-using-kedro/companies-using-kedro.tsx
+++ components/companies-using-kedro/companies-using-kedro.tsx
@@ -8,27 +8,20 @@ export default function CompaniesUsingKedro() {
   return (
     <section className={style.outer}>
       <div className={style.inner}>
-        <div className={style.left}></div>
-        <div className={style.right}>
-          <ul className={style.list}>
-            {companies.map((company) => (
-              <li className={style.company} key={company.name}>
-                <a
-                  href={company.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Image
-                    alt={`${company.name} logo`}
-                    height="120px"
-                    src={company.logo}
-                    width="308px"
-                  />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul className={style.list}>
+          {companies.map((company) => (
+            <li className={style.company} key={company.name}>
+              <a href={company.link} rel="noopener noreferrer" target="_blank">
+                <Image
+                  alt={`${company.name} logo`}
+                  height="120px"
+                  src={company.logo}
+                  width="308px"
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
