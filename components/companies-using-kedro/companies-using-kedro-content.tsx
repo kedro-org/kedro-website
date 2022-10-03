ea55f3b84cb8bbf7c8af41d9568fe4f45293a0e6
Huong Nguyen <32060364+Huongg@users.noreply.github.com>
2022-10-03 10:13:42 +0100

    Feature/use logos layout for companies using kedro (#60)
    * Initial prototype for logo grid layout
    
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>
    
    * Remove unneeded test file
    
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>
    
    * companies logos
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * update order of companies
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * update new design styling using companies logos
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * updated logos
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * layout for mobile/desktop
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * remove left and right
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * increase spacing in mobile version
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * include Our community headline
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * remove link and height from logos
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * increase the header size
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * use mixin for header
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * revert back to the original title size
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>
    Signed-off-by: huongg <huongg1409@gmail.com>
    Co-authored-by: Tynan DeBold <thdebold@gmail.com>

--- components/companies-using-kedro/companies-using-kedro-content.tsx
+++ components/companies-using-kedro/companies-using-kedro-content.tsx
@@ -1,64 +1,82 @@
 export const companies = [
   {
-    name: 'Telkomsel',
-    link: 'https://medium.com/life-at-telkomsel/how-we-build-a-production-grade-data-pipeline-7004e56c8c98',
+    name: 'QuantumBlack',
+    link: 'https://medium.com/quantumblack/introducing-kedro-the-open-source-library-for-production-ready-machine-learning-code-d1c6d26ce2cf',
+    logo: '/images/logos/QB.png',
   },
-  {
-    name: 'Beamery',
-    link: 'https://medium.com/hacking-talent/production-code-for-data-science-and-our-experience-with-kedro-60bb69934d1f',
-  },
-  { name: 'NASA', link: 'https://github.com/nasa/ML-airport-configuration' },
   {
     name: 'Belfius',
     link: 'https://www.linkedin.com/posts/vangansen_mlops-machinelearning-kedro-activity-6772379995953238016-JUmo/',
-  },
-  {
-    name: 'Sber',
-    link: 'https://www.linkedin.com/posts/seleznev-artem_welcome-to-kedros-documentation-kedro-activity-6767523561109385216-woTt/?ref=morioh.com&utm_source=morioh.com',
-  },
-  {
-    name: 'Augment Partners',
-    link: 'https://www.linkedin.com/posts/augment-partners_kedro-cheat-sheet-by-augment-activity-6858927624631283712-Ivqk/?ref=morioh.com&utm_source=morioh.com',
-  },
-  {
-    name: 'McKinsey & Company',
-    link: 'https://www.mckinsey.com/alumni/news-and-insights/global-news/firm-news/kedro-from-proprietary-to-open-source',
-  },
-  {
-    name: 'GetInData',
-    link: 'https://getindata.com/blog/running-machine-learning-pipelines-kedro-kubeflow-airflow',
-  },
-  {
-    name: 'Indicium',
-    link: 'https://medium.com/indiciumtech/how-to-build-models-as-products-using-mlops-part-2-machine-learning-pipelines-with-kedro-10337c48de92',
-  },
-  {
-    name: 'NHS AI Lab',
-    link: 'https://nhsx.github.io/skunkworks/synthetic-data-pipeline',
-  },
-  {
-    name: 'GMO',
-    link: 'https://recruit.gmo.jp/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/blog/kedro_and_mlflow_tracking/',
-  },
-  {
-    name: 'QuantumBlack',
-    link: 'https://medium.com/quantumblack/introducing-kedro-the-open-source-library-for-production-ready-machine-learning-code-d1c6d26ce2cf',
-  },
-  { name: 'XP', link: 'https://youtu.be/wgnGOVNkXqU?t=2210' },
-  {
-    name: 'AI Singapore',
-    link: 'https://makerspace.aisingapore.org/2020/08/leveraging-kedro-in-100e/',
-  },
-  {
-    name: 'Helvetas',
-    link: 'https://www.linkedin.com/posts/lionel-trebuchon_mlflow-kedro-ml-ugcPost-6747074322164154368-umKw',
+    logo: '/images/logos/belfius.png',
   },
   {
     name: 'Leapfrog',
     link: 'https://www.lftechnology.com/blog/ai-pipeline-kedro/',
+    logo: '/images/logos/leapfrog.png',
   },
   {
-    name: 'Naranja',
-    link: 'https://medium.com/quantumblack/odelsa-uses-kedro-for-social-good-3c7e7fc20306',
+    name: 'Beamery',
+    link: 'https://medium.com/hacking-talent/production-code-for-data-science-and-our-experience-with-kedro-60bb69934d1f',
+    logo: '/images/logos/beamery.png',
+  },
+  {
+    name: 'XP',
+    link: 'https://youtu.be/wgnGOVNkXqU?t=2210',
+    logo: '/images/logos/xpinvestments.png',
+  },
+  {
+    name: 'GMO',
+    link: 'https://recruit.gmo.jp/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/engineer/jisedai/blog/kedro_and_mlflow_tracking/',
+    logo: '/images/logos/gmo.png',
+  },
+  {
+    name: 'Augment Partners',
+    link: 'https://www.linkedin.com/posts/augment-partners_kedro-cheat-sheet-by-augment-activity-6858927624631283712-Ivqk/?ref=morioh.com&utm_source=morioh.com',
+    logo: '/images/logos/augment.png',
+  },
+  {
+    name: 'AI Singapore',
+    link: 'https://makerspace.aisingapore.org/2020/08/leveraging-kedro-in-100e/',
+    logo: '/images/logos/aisingapore.png',
+  },
+  {
+    name: 'GetInData',
+    link: 'https://getindata.com/blog/running-machine-learning-pipelines-kedro-kubeflow-airflow',
+    logo: '/images/logos/getindata.png',
+  },
+  {
+    name: 'NHS AI Lab',
+    link: 'https://nhsx.github.io/skunkworks/synthetic-data-pipeline',
+    logo: '/images/logos/nhs.png',
+  },
+  {
+    name: 'Indicium',
+    link: 'https://medium.com/indiciumtech/how-to-build-models-as-products-using-mlops-part-2-machine-learning-pipelines-with-kedro-10337c48de92',
+    logo: '/images/logos/indicium.png',
+  },
+  {
+    name: 'Telkomsel',
+    link: 'https://medium.com/life-at-telkomsel/how-we-build-a-production-grade-data-pipeline-7004e56c8c98',
+    logo: '/images/logos/telkomsel.png',
+  },
+  {
+    name: 'McKinsey & Company',
+    link: 'https://www.mckinsey.com/alumni/news-and-insights/global-news/firm-news/kedro-from-proprietary-to-open-source',
+    logo: '/images/logos/mckinsey.png',
+  },
+  {
+    name: 'NASA',
+    link: 'https://github.com/nasa/ML-airport-configuration',
+    logo: '/images/logos/nasa.png',
+  },
+  {
+    name: 'Sber',
+    link: 'https://www.linkedin.com/posts/seleznev-artem_welcome-to-kedros-documentation-kedro-activity-6767523561109385216-woTt/?ref=morioh.com&utm_source=morioh.com',
+    logo: '/images/logos/sber.png',
+  },
+  {
+    name: 'Helvetas',
+    link: 'https://www.linkedin.com/posts/lionel-trebuchon_mlflow-kedro-ml-ugcPost-6747074322164154368-umKw',
+    logo: '/images/logos/helvetas.png',
   },
 ];
