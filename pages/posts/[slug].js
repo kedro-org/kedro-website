f24c29d9eba24a622b55178de18912da8b4bdaea
Tynan DeBold <thdebold@gmail.com>
2022-12-16 17:13:14 +0000

    Revalidate blog post pages
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -54,6 +54,7 @@ export async function getStaticProps({ params, preview = false }) {
       post: data?.post ?? null,
       morePosts: data?.morePosts ?? null,
     },
+    revalidate: 10,
   };
 }
 
