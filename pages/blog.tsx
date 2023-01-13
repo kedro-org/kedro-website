a0e7757f11897b1dda1c895f3d52786cd66a795f
huongg <huongg1409@gmail.com>
2023-01-13 11:59:50 +0000

    breakpoint for medium-only gap
    Signed-off-by: huongg <huongg1409@gmail.com>

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import Head from 'next/head';
-import Link from 'next/link';
 
 import Header from '../modules/shared/header';
 
