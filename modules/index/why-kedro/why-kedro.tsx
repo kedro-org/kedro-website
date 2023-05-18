4e1626f7f4754d27b689c8d3040294c128ab0530
huongg <huongg1409@gmail.com>
2023-05-18 17:13:04 +0100

    apply sticky header to the section
    Signed-off-by: huongg <huongg1409@gmail.com>

--- modules/index/why-kedro/why-kedro.tsx
+++ modules/index/why-kedro/why-kedro.tsx
@@ -10,18 +10,20 @@ export default function WhyKedro() {
   return (
     <section className={style.outer} id="why-kedro">
       <div className={style.inner}>
-        <h3 className={style.title}>Why Kedro?</h3>
-        <div className={style.mediaWrapper}>
-          <iframe
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            frameBorder="0"
-            height="100%"
-            loading="lazy"
-            src="https://www.youtube-nocookie.com/embed/5-RnVQrM8I8?modestbranding=1&rel=0"
-            title="YouTube video player"
-            width="100%"
-          ></iframe>
+        <div className={style.stickySection}>
+          <h3 className={style.title}>Why Kedro?</h3>
+          <div className={style.mediaWrapper}>
+            <iframe
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              frameBorder="0"
+              height="100%"
+              loading="lazy"
+              src="https://www.youtube-nocookie.com/embed/5-RnVQrM8I8?modestbranding=1&rel=0"
+              title="YouTube video player"
+              width="100%"
+            ></iframe>
+          </div>
         </div>
         <div className={style.contentWrapper}>
           {tabData.map((data, i) => {
