0e10ece7e8310c6de4c6b97092876e0fabb9deba
Huong Nguyen <32060364+Huongg@users.noreply.github.com>
2023-05-23 10:55:54 -0700

    Update why kedro module (#139)
    * general layout
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * apply sticky header to the section
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * remove unused test
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * mobile layout
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * med siz
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * include margin for mobile and tablet so the header doenst get cut off
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * Add gradient block to the top
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * update colours and font sizes
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * Adding gap in the code block
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    * include padding, nit, font weight
    
    Signed-off-by: huongg <huongg1409@gmail.com>
    
    ---------
    
    Signed-off-by: huongg <huongg1409@gmail.com>

--- modules/index/why-kedro/why-kedro.tsx
+++ modules/index/why-kedro/why-kedro.tsx
@@ -1,66 +1,37 @@
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React from 'react';
 import { tabData } from './why-kedro-data';
 
 import style from './why-kedro.module.scss';
 
 export default function WhyKedro() {
-  const [selectedTab, setSelectedTab] = useState(tabData[0]);
-
   return (
     <section className={style.outer} id="why-kedro">
       <div className={style.inner}>
-        <h3 className={style.title}>Why Kedro?</h3>
-        <div className={style.tabsWrapper}>
-          <div className={style.gradient}></div>
-          <ul className={style.list} role="tablist">
-            {tabData.map((item) => {
-              return (
-                <li
-                  aria-selected={item === selectedTab}
-                  key={item.label}
-                  className={
-                    item === selectedTab
-                      ? `${style.selected} ${style.listItem}`
-                      : style.listItem
-                  }
-                  onClick={() => setSelectedTab(item)}
-                  role="tab"
-                >
-                  {item.label}
-                  {item === selectedTab ? (
-                    <motion.div className={style.underline} layoutId="tab" />
-                  ) : null}
-                </li>
-              );
-            })}
-          </ul>
-          <div className={style.gradient}></div>
+        <div className={style.stickySection}>
+          <h3 className={style.title}>Why Kedro?</h3>
+          <div className={style.mediaWrapper}>
+            <iframe
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              frameBorder="0"
+              height="100%"
+              loading="lazy"
+              src="https://www.youtube-nocookie.com/embed/5-RnVQrM8I8?modestbranding=1&rel=0"
+              title="YouTube video player"
+              width="100%"
+            ></iframe>
+          </div>
+          <div className={style.gradientBlock}></div>
         </div>
-        <AnimatePresence exitBeforeEnter initial={false}>
-          <motion.p
-            animate={{ opacity: 1, y: 0 }}
-            className={style.tabContent}
-            exit={{ opacity: 0, y: -20 }}
-            initial={{ opacity: 0, y: 20 }}
-            key={selectedTab ? selectedTab.label : null}
-            role="tabpanel"
-            transition={{ duration: 0.1 }}
-          >
-            {selectedTab.description}
-          </motion.p>
-        </AnimatePresence>
-        <div className={style.mediaWrapper}>
-          <iframe
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            frameBorder="0"
-            height="100%"
-            loading="lazy"
-            src="https://www.youtube-nocookie.com/embed/5-RnVQrM8I8?modestbranding=1&rel=0"
-            title="YouTube video player"
-            width="100%"
-          ></iframe>
+        <div className={style.contentWrapper}>
+          {tabData.map((data, i) => {
+            return (
+              <div className={style.contentBlock} key={i}>
+                <div className={style.label}>{data.label}</div>
+                <div className={style.description}>{data.description}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
