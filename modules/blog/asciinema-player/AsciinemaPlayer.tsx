945376d3ba21054c129cdb35ea853ebaa5899528
tynandebold <thdebold@gmail.com>
2024-01-24 15:22:55 +0000

    Small cleanup
    Signed-off-by: tynandebold <thdebold@gmail.com>

--- modules/blog/asciinema-player/AsciinemaPlayer.tsx
+++ modules/blog/asciinema-player/AsciinemaPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import 'asciinema-player/dist/bundle/asciinema-player.css';
 
 type AsciinemaPlayerProps = {
