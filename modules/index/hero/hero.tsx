7c4db366ac25cc49deb77c812cc7c775fb19eaa9
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-09-25 22:53:47 -0600

    Use inclusive "data analytics" over "data science"
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- modules/index/hero/hero.tsx
+++ modules/index/hero/hero.tsx
@@ -72,11 +72,11 @@ export default function Hero() {
                   </motion.div>
                 </AnimatePresence>
               </div>
-              data science solved
+              data analytics solved
             </div>
           </h1>
           <h2 className={style.subtitle}>
-            Kedro is a toolbox for production-ready data science.
+            Kedro is a toolbox for production-ready data analytics.
           </h2>
           <div className={style.buttons}>
             <motion.div
